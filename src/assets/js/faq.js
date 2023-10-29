44667b32d68bc4f61ecff2d8ce7fc15433213617
formetnicolas <formetnicolas@gmail.com>
2023-10-29 16:32:54 +0100

    v3

---
+++ src/assets/js/faq.js
@@ -0,0 +1,9 @@
+ // FAQ
+ const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+        for (const item of faqItems) {
+            const onClick = () => {
+            item.classList.toggle('active')
+        }
+        item.addEventListener('click', onClick)
+        }
+              
